b75d2020482f40fc6f19a754524663ea2b027b3f
SethNorton06 <40927198+SethNorton06@users.noreply.github.com>
2021-01-05 22:56:20 -0500

    Add files via upload

---
+++ Detox Profile/DetoxProfileDriver.cs
@@ -0,0 +1,233 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project: Project 3 - DNAAnalysis
+//  File Name: DetoxProfileDriver.cs
+//  Description: Performs a detox analysis on a supplied DNA file 
+//  Course: CSCI 3230-001 - Algorithms
+//  Author: Seth Norton, nortonsp@etsu.edu
+//  Created: May 19, 2020
+//  Copyright: Seth Norton, 2020
+//
+///////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The name space for the program which runs a detox report on a DNA file
+/// </summary>
+namespace DetoxProfile
+{
+    /// <summary>
+    /// The class which runs a detox report on a DNA file
+    /// </summary>
+    public class DetoxProfileDriver
+    {
+
+        /// <summary>
+        /// A structure used for storing info on a particular rs number
+        /// </summary>
+        struct DNAInfo
+        {
+            /// <summary>
+            /// The RSID (rs number) of a particular gene
+            /// </summary>
+            public string RSID { get; set; }
+            /// <summary>
+            /// The chromosome of a particular gene
+            /// </summary>
+            public int Chromosome { get; set; }
+            /// <summary>
+            /// The position of a gene
+            /// </summary>
+            public int Position { get; set; }
+            /// <summary>
+            /// The first allele for a gene
+            /// </summary>
+            public string AlleleOne { get; set; }
+            /// <summary>
+            /// The second allele for a gene
+            /// </summary>
+            public string AlleleTwo { get; set; }
+
+            /// <summary>
+            /// Tells whether or not the gene has any permutation. (-/- - no permutation, +/- - one permutation alelle one, -/+ - one permutation alelle two and +/+ - two mutations)
+            /// </summary>
+            public string Mutation { get; set; }
+
+
+        }//end struct DNAInfo
+
+        /// <summary>
+        /// The driver for the main method of the program which will conduct a detox report on the DNA file given
+        /// </summary>
+        /// <param name="args">The arguments supplied to the program</param>
+        public static void Main(string[] args)
+        {
+
+            List<DNAInfo> DNAInfoList = new List<DNAInfo>();    //The list which holds the info for a DNA file
+            DNAInfo da = new DNAInfo();
+            string DNAInfo = "";                                //a string used to grab DNA info
+            int counter = 0;                                    //used to determine when the program should start grabbing rs numbers
+            DNAInfo = Console.ReadLine();
+            while (DNAInfo != null)
+            {
+                DNAInfo = Console.ReadLine();
+                if (counter > 0 && DNAInfo != null)
+                {
+                    da.RSID = DNAInfo.Split('\t')[0];
+                    da.Chromosome = Int32.Parse(DNAInfo.Split('\t')[1]);
+                    da.Position = Int32.Parse(DNAInfo.Split('\t')[2]);
+                    da.AlleleOne = DNAInfo.Split('\t')[3];
+                    da.AlleleTwo = DNAInfo.Split('\t')[4];
+                    DNAInfoList.Add(da);
+                }//end if (counter > 0 && DNAInfo != null)
+                if (counter == 0 && DNAInfo[0] != '#')
+                    counter++;
+
+            }//end  while (DNAInfo != null)
+            //Add the associated allelles
+            Dictionary<string, string> AlelleDictionary = new Dictionary<string, string>    //a dictionary which holds the dominant alleles for the rs values in question
+            {
+                {"rs1048943","T;T"},
+                {"rs4986883","A;A"},
+                {"rs1799814","C;C"},
+                {"rs762551","A;A"},
+                {"rs1056836","A;A"},
+                {"rs1800440","T;T"},
+                {"rs10012","G;G"},
+                {"rs1801272","A;A"},
+                {"rs28399444","AA;AA"},
+                {"rs1799853","C;C"},
+                {"rs12248560","C;C"},
+                {"rs1135840","G;G"},
+                {"rs1065852","G;G"},
+                {"rs16947","G;G"},
+                {"rs2070676","C;C"},
+                {"rs55897648","G;G"},
+                {"rs6413419","G;G"},
+                {"rs2740574","T;T"},
+                {"rs55785340","A;A"},
+                {"rs4986910","A;A"},
+                {"rs12721627","G;G"},
+                {"rs1695","A;A"},
+                {"rs1138272","C;C"},
+                {"rs4880","A;A"},
+                {"rs4986782","C;C"},
+                {"rs1805158","C;C"},
+                {"rs1801280","T;T"},
+                {"rs1799930","G;G"},
+                {"rs1799931","G;G"},
+                {"rs1801279","G;G"},
+                {"rs1208","A;A"},
+                {"rs1057910","A;A"}
+
+            };
+            Dictionary<string, string> VariationDictionary = new Dictionary<string, string>()   //a dictionary which holds the Gene and Variation for the rs numbers in question
+            {               
+                    {"rs1048943","CYP1A1*2C A4889G"},
+                    {"rs4986883","CYP1A1 m3 T3205C"},
+                    {"rs1799814","CYP1A1 C2453A"},
+                    {"rs762551","CYP1A2 164A>C"},
+                    {"rs1056836","CYP1B1 L432V"},
+                    {"rs1800440","CYP1B1 N453S"},
+                    {"rs10012","CYP1B1 R48G	"},
+                    {"rs1801272","CYP2A6*2 1799T>A"},
+                    {"rs28399444","CYP2A6*20"},
+                    {"rs1799853","CYP2C9*2 C430T"},
+                    {"rs12248560","CYP2C19*17"},
+                    {"rs1135840","CYP2D6 S486T"},
+                    {"rs1065852","CYP2D6 100C>T"},
+                    {"rs16947","CYP2D6 2850C>T"},
+                    {"rs2070676","CYP2E1*1B 9896C>G"},
+                    {"rs55897648","CYP2E1*1B 10023G>A"},
+                    {"rs6413419","CYP2E1*4 4768G>A"},
+                    {"rs2740574","CYP3A4*1B"},
+                    {"rs55785340","CYP3A4*2 S222P"},
+                    {"rs4986910","CYP3A4*3 M445T"},
+                    {"rs12721627","CYP3A4*16 T185S"},
+                    {"rs1695","GSTP1 I105V"},
+                    {"rs1138272","GSTP1 A114V"},
+                    {"rs4880","SOD2 A16V"},
+                    {"rs4986782","R187Q"},
+                    {"rs1805158","NAT1 R64W"},
+                    {"rs1801280","NAT2 I114T"},
+                    {"rs1799930","NAT2 R197Q"},
+                    {"rs1799931","NAT2 G286E"},
+                    {"rs1801279","NAT2 R64Q"},
+                    {"rs1208","NAT2 K268R"},
+                    {"rs1057910","CYP2C9*3 A1075C"}
+
+
+            };
+            string report = "Gene And Variation".PadRight(25).PadLeft(20) + "|".PadRight(5) + "rsID".PadRight(10) + "|".PadRight(5) + "Alleles".PadRight(10) + "|".PadRight(5) + "Result".PadRight(10) + "|".PadRight(5) + "Mutation".PadRight(10) + "|".PadRight(5) +  "Type of Mutation".PadRight(28) + "|".PadRight(5) + "\n";   //stores the report generated
+
+
+            List<string> valuesUsed = new List<string>();   //the rs numbers used in the report
+            //Scan through the list of RSID's and find if one matches the rsID in the Allele Dictionary
+            for (int i = 0; i < DNAInfoList.Count; i++)
+            {
+                if (AlelleDictionary.ContainsKey(DNAInfoList[i].RSID))
+                {
+
+                    //find if the alleles in DNAInfoList[i] are mutated from the ones in the Alelle Dictionary
+                    string AlleleOne = "";  //the first allele of the rs number
+                    string AlleleTwo = "";  //the second allele of the rs number
+                    string Mutation = "";   //holds the sign for the mutation
+                    string MutationType = "";//translates what the sign for the mutation means
+                    AlelleDictionary.TryGetValue(DNAInfoList[i].RSID, out AlleleOne);
+                    AlleleTwo = AlleleOne.Split(';')[1];
+                    AlleleOne = AlleleOne.Split(';')[0];
+                    valuesUsed.Add(DNAInfoList[i].RSID);
+                    if (DNAInfoList[i].AlleleOne == AlleleOne)
+                    {
+                        
+                        if (DNAInfoList[i].AlleleTwo == AlleleTwo)
+                        {
+                            Mutation = "-/-";   //No mutation
+                            MutationType = "No mutation";
+                        }//end if(DNAInfoList[i].AlleleTwo == AlleleTwo)
+                        else
+                        {
+                            Mutation = "-/+";   //one mutation allele two
+                            MutationType = "Single Mutation: Allele Two";
+                        }//end else (DNAInfoList[i].AlleleTwo == AlleleTwo)
+                    }//end if (DNAInfoList[i].AlleleOne == AlleleOne)
+                    else
+                    {
+                        if (DNAInfoList[i].AlleleTwo == AlleleTwo)
+                        {
+                            Mutation = "+/-"; //one mutation allele one
+                            MutationType = "Single Mutation: Allele One";
+                        }//end if(DNAInfoList[i].AlleleTwo == AlleleTwo)
+                        else
+                        {
+                            Mutation = "+/+";   //a double mutation
+                            MutationType = "Double Mutation";
+                        }//end else(DNAInfoList[i].AlleleTwo == AlleleTwo)
+                    }// end else(DNAInfoList[i].AlleleOne == AlleleOne)
+                    string variation = "";  //a string to get the gene and variation of an rs number
+                    VariationDictionary.TryGetValue(DNAInfoList[i].RSID, out variation);
+                    report += variation.Trim().PadRight(25).PadLeft(20) + "|".PadRight(5) + DNAInfoList[i].RSID.PadRight(10) + "|".PadRight(5) + DNAInfoList[i].AlleleOne.PadRight(10) + "|".PadRight(5) + DNAInfoList[i].AlleleTwo.PadRight(10) + "|".PadRight(5) + Mutation.PadRight(10) + "|".PadRight(5) + MutationType.PadRight(28) + "|".PadRight(5) + "\n";
+                }
+            }
+            string RSNumbersNotFound = "";  //a string to get the rs numbers not found in the DNA report for a detox profile
+            foreach (KeyValuePair<string, string> entry in VariationDictionary)
+            {
+                if (!valuesUsed.Contains(entry.Key))
+                {
+                    RSNumbersNotFound += entry.Key + " (" + entry.Value.Trim() + ")" + ", ";
+                }
+            }
+            Console.WriteLine(report);
+            Console.WriteLine();
+            Console.WriteLine("RS Numbers and Genes not found in DNA file: ");
+            Console.WriteLine(RSNumbersNotFound);
+            Console.ReadLine();
+        }//end public static void Main(string[] args)
+    }//end public class DetoxProfileDriver
+}//end name space DetoxProfile
---
+++ Methylation Analysis/MethylationAnalysisDriver.cs
@@ -0,0 +1,220 @@
+﻿//////////////////////////////////////////////////////////////////////////////////////
+//
+//  Project: Project 3 - DNAAnalysis
+//  File Name: MethylationAnalysisDriver.cs
+//  Description: Generates a Methylation report based on a DNA file 
+//  Course: CSCI 3230-001 - Algorithms
+//  Author: Seth Norton, nortonsp@etsu.edu
+//  Created: May 19, 2020
+//  Copyright: Seth Norton, 2020
+//
+///////////////////////////////////////////////////////////////////////////////////////
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// The name space for the Methylation analysis
+/// </summary>
+namespace Methylation
+{
+    /// <summary>
+    /// The class which contains the program which runs a Methylation analysis for the supplied DNA file
+    /// </summary>
+    public class MethylationAnalysisDriver
+    {
+        
+        /// <summary>
+        /// A struct which contains the rs info for RS numbers
+        /// </summary>
+        struct DNAInfo
+        {
+            /// <summary>
+            /// The rsid for a gene
+            /// </summary>
+            public string RSID{get;set;}
+            /// <summary>
+            /// The chromosome of the gene/rs number
+            /// </summary>
+            public int Chromosome { get; set; }
+            /// <summary>
+            /// The position of the rs number/gene
+            /// </summary>
+            public int Position { get; set; }
+            /// <summary>
+            /// The first allele of the gene/rs number
+            /// </summary>
+            public char AlleleOne { get; set; }
+            /// <summary>
+            /// The second allele of the gene/rs number
+            /// </summary>
+            public char AlleleTwo { get; set; }
+            /// <summary>
+            /// Shows if the rs number in the text file has a mutation
+            /// </summary>
+            public string Mutation { get; set; }
+
+
+        }//end struct DNAInfo
+        /// <summary>
+        /// The main method which runs the Methylation report of the rs numbers
+        /// </summary>
+        /// <param name="args">The arguments supplied to the program</param>
+        public static void Main(string[] args)
+        {
+
+            List<DNAInfo> DNAInfoList = new List<DNAInfo>();    //a list of all the DNA info
+            DNAInfo da = new DNAInfo();                         //stores the DNA info for an rs number
+            string DNAInfo = "";                                //a string which gets each line of the DNA file
+            int counter = 0;                                    //a counter which is used to determine when we should parse rs numbers
+            DNAInfo = Console.ReadLine();
+            while (DNAInfo != null)
+            {
+                DNAInfo = Console.ReadLine();
+                if (counter > 0 && DNAInfo != null)
+                {
+                    da.RSID = DNAInfo.Split('\t')[0];
+                    da.Chromosome = Int32.Parse(DNAInfo.Split('\t')[1]);
+                    da.Position = Int32.Parse(DNAInfo.Split('\t')[2]);
+                    da.AlleleOne = DNAInfo.Split('\t')[3].ToCharArray()[0];
+                    da.AlleleTwo = DNAInfo.Split('\t')[4].ToCharArray()[0];
+                    DNAInfoList.Add(da);
+                }//end if (counter > 0 && DNAInfo != null)
+                if (counter == 0 && DNAInfo[0] != '#')
+                    counter++;
+
+            }//end while (DNAInfo != null)
+            //Add the associated allelles
+            Dictionary<string, string> AlelleDictionary = new Dictionary<string, string>    //a dictionary which contains the dominant alleles of a rs number
+            {
+                {"rs4680", "G;G" },
+                {"rs4633", "C;C" },
+                {"rs769224", "G;G" },
+                {"rs1544410", "C;C" },
+                {"rs731236", "T;T" },
+                {"rs6323", "A;A" },
+                {"rs3741049", "G;G" },
+                {"rs1801133", "G;G" },
+                {"rs2066470", "T;T" },
+                {"rs1801131", "C;C" },
+                {"rs1805087", "A;A" },
+                {"rs1801394", "A;A" },
+                {"rs10380", "C;C" },
+                {"rs162036", "A;A" },
+                {"rs2287780", "C;C" },
+                {"rs1802059", "G;G" },
+                {"rs567754", "C;C" },
+                {"rs617219", "A;A" },
+                {"rs651852", "C;C" },
+                {"rs819147", "T;T" },
+                {"rs819134", "A;A" },
+                {"rs819171", "T;T" },
+                {"rs234706", "G;G" },
+                {"rs1801181", "G;G" },
+                {"rs2298758", "G;G" },
+                {"rs1979277","G;G" }
+
+            };
+            Dictionary<string, string> VariationDictionary = new Dictionary<string, string>()   //a dictionary containing the variation of particular rs numbers
+            {
+                {"rs4680", "COMT V158M" },
+                {"rs4633", "COMT H62H" },
+                {"rs769224", "COMT P199" },
+                {"rs1544410", "VDR Bsm" },
+                {"rs731236", "VDR Taq" },
+                {"rs6323", "MAO A R297R" },
+                {"rs3741049", "ACAT1-02" },
+                {"rs1801133", "MTHFR C677T" },
+                {"rs2066470", "MTHFR 03 P39P" },
+                {"rs1801131", "MTHFR A1298C" },
+                {"rs1805087", "MTR A2756G" },
+                {"rs1801394", "MTRR A66G" },
+                {"rs10380", "MTRR H595Y" },
+                {"rs162036", "MTRR K350A" },
+                {"rs2287780", "MTRR R415T" },
+                {"rs1802059", "MTRR A664A" },
+                {"rs567754", "BHMT-02" },
+                {"rs617219", "BHMT-04" },
+                {"rs651852", "BHMT-08" },
+                {"rs819147", "AHCY-01" },
+                {"rs819134", "AHCY-02" },
+                {"rs819171", "AHCY-19" },
+                {"rs234706", "CBS C699T" },
+                {"rs1801181", "CBS A360A" },
+                {"rs2298758", "CBS N212N" },
+                {"rs1979277","SHMT1 C1420T" }
+            };
+            //the string which contains the report of the methylation analysis
+            string report = "Gene And Variation".PadRight(20).PadLeft(20) + "|".PadRight(5) + "rsID".PadRight(10) + "|".PadRight(5) + "Alleles".PadRight(10) + "|".PadRight(5) + "Result".PadRight(10) + "|".PadRight(5) + "Mutation".PadRight(10) + "|".PadRight(5) + "Type of Mutation".PadRight(28) + "|".PadRight(5) + "\n";
+
+
+
+
+
+            List<string> valuesUsed = new List<string>();   //the values used for the methylation analysis
+            //Scan through the list of RSID's and find if one matches the rsID in the Allele Dictionary
+            for (int i = 0; i < DNAInfoList.Count; i++)
+            {
+                if (AlelleDictionary.ContainsKey(DNAInfoList[i].RSID))
+                {
+
+                    //find if the alleles in DNAInfoList[i] are mutated from the ones in the Alelle Dictionary
+                    string AlleleOne = "";
+                    string AlleleTwo = "";
+                    string Mutation = "";
+                    string MutationType = "";
+                    valuesUsed.Add(DNAInfoList[i].RSID);
+                    AlelleDictionary.TryGetValue(DNAInfoList[i].RSID, out AlleleOne);
+                    AlleleTwo = AlleleOne.Split(';')[1];
+                    AlleleOne = AlleleOne.Split(';')[0];
+                    if (DNAInfoList[i].AlleleOne == AlleleOne[0])
+                    {
+                        if(DNAInfoList[i].AlleleTwo == AlleleTwo[0])
+                        {
+                            Mutation = "-/-";   //No mutation
+                            MutationType = "No mutation";
+                        }//end if(DNAInfoList[i].AlleleTwo == AlleleTwo[0])
+                        else
+                        {
+                            Mutation = "-/+";
+                            MutationType = "Single Mutation: Allele Two";
+                        }//end else(DNAInfoList[i].AlleleTwo == AlleleTwo[0])
+                    }//end if (DNAInfoList[i].AlleleOne == AlleleOne[0])
+                    else
+                    {
+                        if (DNAInfoList[i].AlleleTwo == AlleleTwo[0])
+                        {
+                            Mutation = "+/-";
+                            MutationType = "Single Mutation: Allele One";
+                        }//end if(DNAInfoList[i].AlleleTwo == AlleleTwo[0])
+                        else
+                        {
+                            Mutation = "+/+";
+                            MutationType = "Double Mutation";
+                        }//end else(DNAInfoList[i].AlleleTwo == AlleleTwo[0])
+                    }//end else(DNAInfoList[i].AlleleOne == AlleleOne[0])
+                    string variation = "";  //stores the gene variation of the rs number
+                    VariationDictionary.TryGetValue(DNAInfoList[i].RSID, out variation);
+                    report += variation.PadRight(20).PadLeft(20) + "|".PadRight(5) + DNAInfoList[i].RSID.PadRight(10) + "|".PadRight(5) + DNAInfoList[i].AlleleOne.ToString().PadRight(10) + "|".PadRight(5) + DNAInfoList[i].AlleleTwo.ToString().PadRight(10) + "|".PadRight(5) + Mutation.PadRight(10) + "|".PadRight(5) + MutationType.PadRight(28) + "|".PadRight(5) + "\n";
+                }//end if (AlelleDictionary.ContainsKey(DNAInfoList[i].RSID))
+            }//end for (int i = 0; i < DNAInfoList.Count; i++)
+            string RSNumbersNotFound = "";  //string which contains the rs numbers not found in the DNA file
+            foreach(KeyValuePair<string, string> entry in VariationDictionary)
+            {
+                if(!valuesUsed.Contains(entry.Key))
+                {
+                    RSNumbersNotFound += entry.Key + " (" + entry.Value.Trim() +")" + ", ";
+                }//end if(!valuesUsed.Contains(entry.Key))
+            }//end  foreach(KeyValuePair<string, string> entry in VariationDictionary)
+            Console.WriteLine(report);
+            Console.WriteLine();
+            Console.WriteLine("RS Numbers and Genes not found in DNA file: ");
+            Console.WriteLine(RSNumbersNotFound);
+            Console.ReadLine();
+        }//end public static void Main(string[] args)
+    }//end public class MethylationAnalysisDriver
+}//end name space Methylation
